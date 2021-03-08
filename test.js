3e66223e21af7e6abc24a291e2bee7cbbf996b65
Nick Fox-Gieg <nick@fox-gieg.com>
2021-03-08 15:48:45 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 // Note that this example is for the original Max js object, which only supports ES5.
 // For ES6, use the newer node.script object.
 // https://cycling74.com/forums/any-plans-to-update-support-for-recent-versions-of-js
+// https://docs.cycling74.com/max5/refpages/max-ref/js.html
 
 "use strict";
 
